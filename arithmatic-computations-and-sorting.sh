286bf99dd2f025830f5ffec318e608a48f984124
iamatharv <iamatharvaj@gmail.com>
2021-04-08 19:35:30 +0530

    Added: Take three inputs a, b, c from user

--- arithmatic-computations-and-sorting.sh
+++ arithmatic-computations-and-sorting.sh
@@ -2,5 +2,9 @@
 
 echo "Welcome to arithmatic computations and sorting"
 
-
+#Use-case-1
+read -p "Enter three numbers a, b, c: " a b c
+echo "Value of a= $a"
+echo "Value of b= $b"
+echo "Value of c= $c"
 
